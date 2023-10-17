ac69d25ca59171be8d1963143731ec627221984b
Iván Lorenzo Mediavilla <ivanlmediavilla@gmail.com>
2023-10-17 20:55:14 +0200

    added sqlite3 database, thinking about adding sequelize orm

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const sqlite3 = require('sqlite3').verbose()
+let sql
+
+// Connection to database
+const db = new sqlite3.Database('../../loan_api.db', sqlite3.OPEN_READWRITE,
+  (err) => {
+    if (err) return console.log(err.message)
+  }
+)
