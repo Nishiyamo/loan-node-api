8802a6efa5aec31a2607041de7764412dcae6b39
Iván Lorenzo Mediavilla <ivanlmediavilla@gmail.com>
2023-10-18 13:30:24 +0200

    swagger working

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -1,5 +1,7 @@
 module.exports = app => {
+  const { swaggerDocs } = require('../v1/swagger')
   app.listen(app.get('port'), () => {
     console.log('API is listening on port', app.get('port'))
+    swaggerDocs(app, app.get('port'))
   })
 }
---
+++ src/v1/swagger.js
@@ -0,0 +1,28 @@
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Loan Simulator API',
+      version: '1.0.0'
+    }
+  },
+  apis: [
+    'src/v1/routes/loanRoutes.js',
+    'src/v1/routes/userRoutes.js'
+  ]
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+const swaggerDocs = (app, port) => {
+  app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.get('/api/v1/docs.json', (res, req) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerDocs)
+  })
+}
+
+module.exports = { swaggerDocs }
